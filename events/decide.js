87d781784f8e61f9152d01ecada95c8f7dea8106
Peter Bailey <peterkbailey@cmail.carleton.ca>
2021-05-06 22:18:25 -0400

    fix removing one message capability

--- events/decide.js
+++ events/decide.js
@@ -1,19 +1,20 @@
 async function execute(message, args){
     let options;
     if(args.length > 0){
-        options = args;
-    } else {
-        message.reply("What are the options?");
-
-        filter = function(m){
-            return m.member.id === message.member.id;
-        };
-    
-        let collectedMessages = await message.channel.awaitMessages(filter, {max:1});
-        for(let message of collectedMessages){
-            options = message[1].content.split(',');
-        }
+        // options = args;
+        message.reply("That doesn't work yet, send the options in a 2nd message!");
     }
+    message.reply("What are the options?");
+
+    filter = function(m){
+        return m.member.id === message.member.id;
+    };
+
+    let collectedMessages = await message.channel.awaitMessages(filter, {max:1});
+    for(let message of collectedMessages){
+        options = message[1].content.split(', ');
+    }
+    
 
     let choice = Math.floor(Math.random()*options.length);
     message.reply("I choose " + options[choice] +'!');
