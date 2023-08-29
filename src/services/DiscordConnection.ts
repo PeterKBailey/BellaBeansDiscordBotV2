2e629f683e436bba9086058c41fd3e433e8790a0
Peter Bailey <peterkbailey@cmail.carleton.ca>
2023-08-28 22:56:52 -0400

    Resolve memory problems when indexing

--- src/services/DiscordConnection.ts
+++ src/services/DiscordConnection.ts
@@ -1,4 +1,6 @@
-import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import { Client, GatewayIntentBits, Partials, Message, TextBasedChannel } from 'discord.js';
+import v8 from 'node:v8';
+
 require("dotenv").config();
 
 /**
@@ -30,5 +32,65 @@ export class DiscordConnection {
         }
 
         return this.client;
+    }
+
+    /**
+     * Processes channel messages, based on https://stackoverflow.com/a/71620968
+     * @param channel the channel with messages being processed
+     * @param maxNumToProcess how many messages should be processed, -1 for all
+     * @param task the processing being performed on each message
+     * @param returnResults if the results of processing should be returned
+     * @returns the results of the processing if requested - this could drastically increase memory usage
+     */
+    public static async processChannelMessages<T>(channel: TextBasedChannel, maxNumToProcess: number, task: (message: Message) => Promise<T>, returnResults: boolean): Promise<T[]> {
+        // return nothing if there are no messages to process
+        if(!channel.messages || maxNumToProcess === 0) return [];
+
+        maxNumToProcess = Math.floor( maxNumToProcess );
+
+        // fetch the first message from the channel
+        let messagePage = await channel.messages.fetch({ limit: 1 });
+        let message = messagePage.size === 1 ? messagePage.at(0) : null;
+
+        let tasks: Promise<T>[] = [];
+        let results: T[] = [];
+
+        // process the first message found
+        if(!message) return [];
+        tasks.push(task(message));
+    
+        // fetch 100 messages prior to the current message as long as there are still messages being retrieved
+        let count = 1;
+        while (message) {
+            messagePage = await channel.messages.fetch({ limit: 100, before: message.id })
+            for(const messageTuple of messagePage){
+                // if maxNumToProcess is below 0 this will never hit
+                if(count === maxNumToProcess) break;
+                tasks.push(task(messageTuple[1]));
+                count++;
+            };
+
+            // only await when we need to...
+            if(this.getPercentOfMaxHeapInUse() > 0.8){
+                const localResults = await Promise.all(tasks);
+                results = returnResults ? results.concat(localResults) : [];
+                // if we still have a problem then time to throw before we crash
+                if(this.getPercentOfMaxHeapInUse() > 0.8){
+                    throw new Error("Out of memory!!");
+                }
+            }
+
+            // Update the message pointer to be the last message on the page of messages
+            message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+        }
+
+        const finalResults = await Promise.all(tasks);
+        return returnResults ? results.concat(finalResults) : [];
+    }
+    
+    // get the percentage of the max heap currently in use
+    private static getPercentOfMaxHeapInUse() {
+        const maxHeap = v8.getHeapStatistics().heap_size_limit;
+        return v8.getHeapStatistics().used_heap_size / maxHeap;
     }
 }
